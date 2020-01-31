383ffec5425465e974b8fb3cf90dd36146ea87b1
Luigioo <32347376+Luigioo@users.noreply.github.com>
2020-01-31 15:47:50 +0000

    finished

--- mips/mips1.asm
+++ mips/mips1.asm
@@ -4,44 +4,82 @@ packets: # contains four example packet headers
 .word 0x45000030 0x44224000 0x80060000 0x8c7c19ac 0xae241e2b
 .word 0x45000030 0x44224000 0x8006442e 0x8c7c19ac 0xae241e2b
 .word 0x45000031 0x44224000 0x8006442e 0x8c7c19ac 0xae241e2b
+beginWord: .asciiz "\nhello\n"
+endWord: .asciiz "\nend of program\n"
+sep: .asciiz "\n---\n"
+ssep: .asciiz "\nis it FFFF?\n"
+
 
 .text
+	li $v0, 4
+	la $a0, beginWord
+	syscall
+	
 	j    main		#starting point of the program
 
+four:	
+	li $t7, 20
+	mul $t7, $t7, $t0	#now t7 is our offset between ips
+	
+
+	
+	addu $s1, $t7, $s0
+	
+	li $t3, 0
+	li $t2, 0
+	
 check:
-        # TODO: here would go the checksum verification function
-        add $s0, $a0, $zero
-	lhu $t0, 0($s0)
-	add $t1, $t1, $t0
+
+	lhu $t1, 0($s1)
+	add $t2, $t2, $t1
 	
-	addi $s0, $s0, 2
+	addi $s1, $s1, 2
 	
-	addi $v1, $v1, 1
-	blt $v1, 5, check
-	sw $s0, 80($a0)
-	jr   $ra
+
+	
+	addi $t3, $t3, 1
+	blt $t3, 10, check
+	
+#end of check
+	li $t4, 0
+	li $t5, 0
+	srl $t4, $t2, 16
+	sll $t5, $t2, 16
+	srl $t5, $t5, 16
+	
+	
+	
+	add $t4, $t4, $t5
+	
+	li $v0, 34
+	addi $a0, $t4, 0
+	syscall
+	li $v0, 4
+	la $a0, ssep
+	syscall
+	
+	
+	addi $t0, $t0, 1	#jump back to four
+	blt $t0,4,four
+	jr $ra
 
 main:	la   $s0,packets	 # fetch address of packet buffer
-
-	addi $t0, $zero, 0
-	
+	li $t0, 0
 	jal four
 	j end
 	
 
 	#TODO: here would go a loop that calls
-four:	
-	
-	
 
-	addi $a0, $a0, 0x0014
-	
-	lui $v1, 0x0000
-	ori $v1, $v1, 0x0000
-	jal check
-	addi $v0, $v0, 1
-	blt $v0,4,four
-	jr $ra
 	#      check for each packet header
 
-end:	nop
+
+        # TODO: here would go the checksum verification function
+
+end:	
+	
+	li $v0, 4
+	la $a0, endWord
+	syscall
+	li $v0, 10
+	syscall
